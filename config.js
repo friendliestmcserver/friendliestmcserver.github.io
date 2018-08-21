cc6af1e31c791b501aae2049961d9ee8e94d3821
friendliestmcserver <animexminecraftyeet@gmail.com>
2018-08-21 01:33:00 +0000

    add index & js files

---
+++ config.js
@@ -0,0 +1,105 @@
+var CONFIG = {
+  "maps": {
+    "cave": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1534477321,
+        1534477321,
+        1534477321,
+        1534477321
+      ],
+      "maxZoom": 11,
+      "name": "animeland caves",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "animeland_isometric_t1",
+      "tileSize": 384,
+      "world": "animeland",
+      "worldName": "animeland",
+      "worldSeaLevel": 64
+    },
+    "day_normal": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1534477321,
+        1534477321,
+        1534477321,
+        1534477321
+      ],
+      "maxZoom": 11,
+      "name": "animeland daytime",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "animeland_isometric_t1",
+      "tileSize": 384,
+      "world": "animeland",
+      "worldName": "animeland",
+      "worldSeaLevel": 64
+    },
+    "night_normal": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1534477321,
+        1534477321,
+        1534477321,
+        1534477321
+      ],
+      "maxZoom": 11,
+      "name": "animeland night",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "animeland_isometric_t1",
+      "tileSize": 384,
+      "world": "animeland",
+      "worldName": "animeland",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "day_normal",
+    "night_normal",
+    "cave"
+  ],
+  "tileSetGroups": {
+    "animeland_isometric_t1": {
+      "maxZoom": 11,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
